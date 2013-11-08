6530a5e46e9cd7cb72a00ec59a29f8a38a8a336e
dadalala <a0072099@nus.edu.sg>
2013-11-09 01:28:22 +0800

    added multithread for DetectLossService service

--- src/com/example/phonefinder/DetectLossService.java
+++ src/com/example/phonefinder/DetectLossService.java
@@ -23,9 +23,51 @@ public class DetectLossService extends Service {
 		Toast.makeText(getApplicationContext(), "Your Device is Starting to be protected",
 				   Toast.LENGTH_LONG).show();
     
-	
+		Thread mBackground1;
+		Thread mBackground2;
+		
+		mBackground1 = new Thread(new Runnable()
+        {
+                // Setup the run() method that is called when the background thread
+                // is started.
+                public void run()
+                {
+                        // Do you background thread process here...
+                        // In my case, strMsgRcvd is a String where I store
+                        // the message received via my socket.
 
-	
+                	for(int i = 0; i < 10; i++)
+                	{
+                		Log.d("DetectLossService", "first tread test: " + i);
+                		
+                	}
+                
+
+                }
+        });
+		
+		mBackground2 = new Thread(new Runnable()
+        {
+                // Setup the run() method that is called when the background thread
+                // is started.
+                public void run()
+                {
+                        // Do you background thread process here...
+                        // In my case, strMsgRcvd is a String where I store
+                        // the message received via my socket.
+
+                	for(int i = 0; i < 10; i++)
+                	{
+                		Log.d("DetectLossService", "second tread test: " + i);
+                		
+                	}
+                
+
+                }
+        });
+		
+		mBackground1.start();
+		mBackground2.start();
 	
 	}
 
